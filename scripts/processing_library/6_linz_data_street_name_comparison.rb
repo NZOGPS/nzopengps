542d3cfdd143dede690d21592d348561ad743ef8
Sam Giffney <samuelgiffney@gmail.com>
2012-01-31 11:15:03 +0000

    linz street name comparison

---
+++ scripts/processing_library/6_linz_data_street_name_comparison.rb
@@ -0,0 +1,84 @@
+=begin
+
+read the LINZ Data Service output tile and the NZOGPS tile and report on the difference in street names
+
+expects that the LinzDataService data has been processed already to form the -LINZ.mp tiles
+=end
+
+def process_polish_buffer(buffer)
+  linzid, street_name, street_name_2 = nil
+  
+	buffer.each{|line|
+    if line =~ /;linzid\=(\d*)/ then
+      linzid = $1
+    elsif line =~ /Label\=(.*)/ then
+      street_name = $1
+    elsif line =~ /Label2\=(.*)/ then
+      street_name_2 = $1
+    end
+	}
+  
+  if !@linz_street_names_by_linzid.has_key?(linzid) then
+    return
+  end
+  
+  official_street_name = @linz_street_names_by_linzid[linzid]
+  match = false
+  
+  #standardise whitespace and case
+  official_street_name = official_street_name.squeeze(' ').strip.upcase
+  street_name = "#{street_name}".squeeze(' ').strip.upcase
+  street_name_2 = "#{street_name_2}".squeeze(' ').strip.upcase
+  
+  if official_street_name == street_name then
+    match = true
+  elsif official_street_name == street_name_2 then
+    match = true
+  elsif street_name =~ /~\[0x2d\](.+)/i || street_name_2 =~ /~\[0x2d\](.+)/i then
+    highway_number = $1
+    if official_street_name =~ /STATE HIGHWAY #{highway_number}/ then
+      match = true
+    end
+  end
+    
+  if !match then 
+    print "#{linzid}\t#{street_name}\t#{street_name_2}\t#{official_street_name}\n"
+    @reporting_file.print "#{linzid}\t#{street_name}\t#{street_name_2}\t#{official_street_name}\n"
+  end
+  
+end
+
+def pre_processing()
+  
+  @linz_street_names_by_linzid = {}
+  linz_data_service_file = File.join(@base, '..', 'LinzDataService', 'outputslinz', "#{@tile}-LINZ.mp")
+  if !File.exists?(linz_data_service_file) then
+    raise "Unable to find required LINZ data file at #{linz_data_service_file}\n"
+  end
+  
+  linzid, street_name = nil
+  File.open(linz_data_service_file).each{|line|
+    if line =~ /;linzid\=(\d+)/ then
+      linzid = $1
+    elsif line =~ /Label\=(.*)/ then
+      street_name = $1
+    end
+    
+    if (line =~ /^\[END(.*)/) then
+      if linzid && street_name then
+        @linz_street_names_by_linzid[linzid] = street_name
+      end
+      linzid, street_name = nil
+    end
+  }
+  print "#{@linz_street_names_by_linzid.size} distinct linz ids found in #{linz_data_service_file}\n"
+
+    print "linzid\tstreet_name\tstreet_name_2\tofficial_street_name\n"
+    @reporting_file.print "linzid\tstreet_name\tstreet_name_2\tofficial_street_name\n"
+end
+
+def post_processing()
+  
+  @reporting_file.print "#############################\n\n"
+  
+end

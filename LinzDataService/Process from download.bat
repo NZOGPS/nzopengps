9a3055a178d302744bb6902f9eae11f39b0e1be2
Gary Turner <gary.c.turner@gmail.com>
2013-05-01 13:02:04 +0000

    Delocalise scripts

--- LinzDataService/Process from download.bat
+++ LinzDataService/Process from download.bat
@@ -1,8 +1,13 @@
+@echo off
+if not exist ..\setlocals.bat echo ..\setlocals.bat not found. You need to copy and customise the sample file & goto :eof
+call ..\setlocals.bat
+@echo on
 start tortoiseproc /command:update /path:".." /closeonend:1
-move "C:\Documents and Settings\turner\My Documents\Downloads\lds-new-zealand-2layers-SHP.zip" .
-wzunzip lds-new-zealand-2layers-SHP -o lds-nz-street-address-electoral-SHP	*\nz-street-address-elector.*
-wzunzip lds-new-zealand-2layers-SHP -o lds-nz-road-centre-line-electoral-SHP	*\nz-road-centre-line-elect.*
-perl renzip.pl
+@if not exist %nzogps_download%"lds-new-zealand-2layers-SHP.zip" echo new shapefile download not found. & goto :eof
+move %nzogps_download%"lds-new-zealand-2layers-SHP.zip" .
+%nzogps_unzip_cmd% lds-new-zealand-2layers-SHP -o lds-nz-street-address-electoral-SHP	*\nz-street-address-elector.*
+%nzogps_unzip_cmd% lds-new-zealand-2layers-SHP -o lds-nz-road-centre-line-electoral-SHP	*\nz-road-centre-line-elect.*
+%nzogps_perl_cmd% renzip.pl
 :xx
 cd ..\scripts\postgres
 call update.bat
--- scripts/postgres/update.bat
+++ scripts/postgres/update.bat
@@ -1,11 +1,10 @@
 setlocal
-set psql=C:\Program Files\PostgreSQL\9.1\bin\
-net start "postgresql-9.1 - PostgreSQL Server 9.1"
+net start %nzogps_psql_svc%
 time /t
-"%psql%shp2pgsql" -d -D -s4167 "..\..\LinzDataService\lds-nz-street-address-electoral-SHP\nz-street-address-elector.shp"  nz-street-address-elector > nz-street-address.sql
+"%nzogps_psql_bin%shp2pgsql" -d -D -s4167 "..\..\LinzDataService\lds-nz-street-address-electoral-SHP\nz-street-address-elector.shp"  nz-street-address-elector > nz-street-address.sql
 time /t
-"%psql%psql" -U postgres -d nzopengps < nz-street-address.sql
+"%nzogps_psql_bin%psql" -U postgres -d nzopengps < nz-street-address.sql
 time /t
-"%psql%psql" -U postgres -d nzopengps < postproc.sql
+"%nzogps_psql_bin%psql" -U postgres -d nzopengps < postproc.sql
 time /t
 touch ..\database.date

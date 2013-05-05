a4eb23ebc70118b90abccd666422441ec8540a44
Gary Turner <gary.c.turner@gmail.com>
2013-05-05 08:26:34 +0000

    explicitly set geometry column name

--- scripts/postgres/update.bat
+++ scripts/postgres/update.bat
@@ -1,7 +1,7 @@
 setlocal
 net start %nzogps_psql_svc%
 time /t
-%nzogps_psql_bin%shp2pgsql -d -D -s4167 "..\..\LinzDataService\lds-nz-street-address-electoral-SHP\nz-street-address-elector.shp"  nz-street-address-elector > nz-street-address.sql
+%nzogps_psql_bin%shp2pgsql -d -g the_geom -D -s4167 "..\..\LinzDataService\lds-nz-street-address-electoral-SHP\nz-street-address-elector.shp"  nz-street-address-elector > nz-street-address.sql
 time /t
 %nzogps_psql_bin%psql -U postgres -d nzopengps < nz-street-address.sql
 time /t

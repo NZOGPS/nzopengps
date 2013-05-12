83a61b54b993c404350d464c076570146974af37
Gary Turner <gary.c.turner@gmail.com>
2013-05-12 13:02:50 +0000

    Canterbury - forum + LINZ updates. Tweaks to Process from download.bat

--- LinzDataService/Process from download.bat
+++ LinzDataService/Process from download.bat
@@ -6,13 +6,15 @@ start tortoiseproc /command:update /path:".." /closeonend:1
 @if not exist %nzogps_download%"lds-new-zealand-2layers-SHP.zip" echo new shapefile download not found. & goto :eof
 move %nzogps_download%"lds-new-zealand-2layers-SHP.zip" .
 %nzogps_unzip_cmd% lds-new-zealand-2layers-SHP -o lds-nz-street-address-electoral-SHP	*\nz-street-address-elector.*
+if %ERRORLEVEL% GTR 0 echo Street address files not found in zip file & goto :eof
 %nzogps_unzip_cmd% lds-new-zealand-2layers-SHP -o lds-nz-road-centre-line-electoral-SHP	*\nz-road-centre-line-elect.*
+if %ERRORLEVEL% GTR 0 echo Road centre line  files not found in zip file & goto :eof
 %nzogps_perl_cmd% renzip.pl
 :xx
 cd ..\scripts\postgres
 call update.bat
 cd ..
-start GenerateNumbers.bat
+start /low GenerateNumbers.bat
 cd ..\linzdataservice
 ruby shape-parser.rb
 cd ..\scripts

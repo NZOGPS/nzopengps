a1063afee303b7d50c7a87d876946e49fa08471d
Gary Turner <gary.c.turner@gmail.com>
2013-06-19 23:46:28 +0000

    LINZ Update - numbering

--- LinzDataService/renzip.pl
+++ LinzDataService/renzip.pl
@@ -1,10 +1,13 @@
+use strict;
+use File::Basename;
+
 my $fn = shift || "lds-new-zealand-2layers-SHP.zip";
 die "$fn not found\n" if not -f $fn;
 my @stats = stat $fn;
 my @lt = localtime $stats[9];
 my $datebit = sprintf "-%04d-%02d-%02d",$lt[5]+1900,$lt[4],$lt[3];
-my $nfn = $fn;
+my $nfn = basename($fn);
 
 $nfn =~ s/\.zip/$datebit\.zip/;
-
-exec "ren $fn $nfn\n";
+print "command is: ren $fn $nfn\n";
+exec "ren $fn $nfn";

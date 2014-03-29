558d706e428bc1abdc0fbf1d711ac491ae162998
Gary Turner <gary.c.turner@gmail.com>
2014-03-29 13:06:23 +0000

    LINZ updates, tweak to update.bat to check right projection

--- scripts/postgres/update.bat
+++ scripts/postgres/update.bat
@@ -1,7 +1,10 @@
+@echo off
 setlocal
 if not exist ..\..\setlocals.bat echo setlocals.bat not found. You need to copy and customise the sample file & goto :eof
 call ..\..\setlocals.bat
 net start %nzogps_psql_svc%
+grep -q  "^GEOGCS\[\"GCS_NZGD_2000" "..\..\LinzDataService\lds-nz-street-address-electoral-SHP\nz-street-address-elector.prj" 
+if errorlevel 1 echo Wrong projection  & pause & exit 1
 time /t
 %nzogps_psql_bin%shp2pgsql -d -g the_geom -D -s4167 "..\..\LinzDataService\lds-nz-street-address-electoral-SHP\nz-street-address-elector.shp"  nz-street-address-elector > nz-street-address.sql
 time /t

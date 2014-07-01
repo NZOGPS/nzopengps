d58121f5b8fe9466b7bb3f1485068c04fe9f465a
Gary Turner <gary.c.turner@gmail.com>
2014-07-01 04:38:46 +0000

    rna_id/linzid remapping program

---
+++ scripts/remap_linzids.pl
@@ -0,0 +1,45 @@
+use strict;
+
+my $mappingfilename = "..\\LinzDataService\\rna_mappings.csv";
+my %idmap;
+my $cnt;
+my $newfn;
+my $oldid;
+
+$idmap{0}=0;
+
+open IDF, $mappingfilename or die "Cannot find id mapping file $mappingfilename\n";
+while (<IDF>){
+	if (/(\d+),(\d+)/){
+		if ($idmap{$1}){
+			die "map for $1 already set to $idmap{$1} Duplicate on line $.\n";
+		}
+		$idmap{$1}=$2;
+		$cnt++;
+	}
+}
+print "$cnt mappings set\n";
+close IDF;
+$cnt = 0;
+
+die "No filename specified" if ($ARGV[0] eq "");
+
+open MAPF, $ARGV[0] or die "Cannot find map $ARGV[0]\n";
+$newfn = $ARGV[0];
+$newfn =~ s!\.(?=[^.]*$)!\.remapped\.!;
+print "Creating new file $newfn\n";
+open MAPPEDF,">",$newfn or die "Cannot create new map file $newfn\n";
+while (<MAPF>){
+	if (/linzid=(\d+)/){
+		$oldid = $1;
+		if (defined($idmap{$oldid})) {
+			s/$oldid/$idmap{$oldid}/;
+			$cnt++;
+		} else {
+			print "ERROR! linzid map for $1 not found! File $ARGV[0] line $.\n";
+			s/$oldid/###$oldid### - no new mapping for linzid found/;			
+		}  
+	}
+	print MAPPEDF $_;
+}
+print "$cnt linzids remapped\n";

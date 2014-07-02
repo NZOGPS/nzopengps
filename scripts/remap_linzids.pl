4762605c06b7555c6d2e1bfbd61b259b1bfd1690
Gary Turner <gary.c.turner@gmail.com>
2014-07-02 02:38:34 +0000

    Southland updated linzids

--- scripts/remap_linzids.pl
+++ scripts/remap_linzids.pl
@@ -1,17 +1,50 @@
 use strict;
-
-need to do: 
-	paper roads files
-		3 x files! 
-	linzid2s
+use File::Basename;
 	
-my $mappingfilename = "..\\LinzDataService\\rna_mappings.csv";
+my $lds = "..\\LinzDataService\\";
+my $pdn = "${lds}PaperRoads\\";
+my $mappingfilename = "${lds}rna_mappings.csv";
 my %idmap;
 my $cnt;
+my $oldfn;
 my $newfn;
+my $basefile;
+my $basedir;
+my $basesuff;
 my $oldid;
+my $mode;
 
 $idmap{0}=0;
+
+sub do_paper_file{
+	if (-r $oldfn){
+		$cnt = 0;
+		$newfn = $oldfn;
+		$newfn =~ s!\.(?=[^.]*$)!\.remapped\.!;
+		print "Creating new file $newfn\n";
+		open INF, $oldfn or die "Cannot open Paper road file $oldfn\n";
+		open MAPPEDF,">",$newfn or die "Cannot create new map file $newfn\n";
+		while (<INF>){
+			if (/(\d+)\t/){
+				$cnt++;
+				$oldid = $1;
+				if (defined($idmap{$oldid})) {
+					s/^$oldid/$idmap{$oldid}/;
+					$cnt++;
+				} else {
+					print "ERROR! linzid map for $1 not found! File $oldfn line $.\n";
+					s/^$oldid/###$oldid### - no new mapping for linzid found/;			
+				}  
+			}
+			print MAPPEDF $_;
+		}
+		close INF;
+		close MAPPEDF;
+		print "$cnt linzids remapped\n";
+	} else {
+		print "$oldfn not found. Skipped\n";
+	}
+}
 
 open IDF, $mappingfilename or die "Cannot find id mapping file $mappingfilename\n";
 while (<IDF>){
@@ -28,23 +61,37 @@ close IDF;
 $cnt = 0;
 
 die "No filename specified" if ($ARGV[0] eq "");
+$oldfn = $ARGV[0];
+($basefile, $basedir, $basesuff) = fileparse($oldfn,qr/\.[^.]*/);
 
-open MAPF, $ARGV[0] or die "Cannot find map $ARGV[0]\n";
-$newfn = $ARGV[0];
+open INF, $oldfn or die "Cannot find map $oldfn\n";
+$newfn = $oldfn;
 $newfn =~ s!\.(?=[^.]*$)!\.remapped\.!;
 print "Creating new file $newfn\n";
 open MAPPEDF,">",$newfn or die "Cannot create new map file $newfn\n";
-while (<MAPF>){
-	if (/linzid=(\d+)/){
+while (<INF>){
+	if (/linzid\d?=(\d+)/){
 		$oldid = $1;
 		if (defined($idmap{$oldid})) {
 			s/$oldid/$idmap{$oldid}/;
 			$cnt++;
 		} else {
-			print "ERROR! linzid map for $1 not found! File $ARGV[0] line $.\n";
+			print "ERROR! linzid map for $1 not found! File $oldfn line $.\n";
 			s/$oldid/###$oldid### - no new mapping for linzid found/;			
 		}  
 	}
 	print MAPPEDF $_;
 }
+close INF;
+close MAPPEDF;
 print "$cnt linzids remapped\n";
+
+#Paper roads
+
+$oldfn = "${pdn}${basefile}.txt";
+$mode = "Paper road";
+do_paper_file;
+
+$oldfn = "${pdn}${basefile}PaperNumbers.txt";
+$mode = "Paper Numbers";
+do_paper_file;

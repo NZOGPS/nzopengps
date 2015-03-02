5ecc72736f7df75c59c8e83c2ed2c07342e46390
Gary Turner <gary.c.turner@gmail.com>
2015-03-02 23:43:49 +0000

    fix typo eexit

--- scripts/postgres/update.bat
+++ scripts/postgres/update.bat
@@ -4,7 +4,7 @@ if not exist ..\..\setlocals.bat echo setlocals.bat not found. You need to copy 
 call ..\..\setlocals.bat
 net start %nzogps_psql_svc%
 grep -q  "^GEOGCS\[\"GCS_NZGD_2000" "..\..\LinzDataService\lds-nz-street-address-electoral-SHP\nz-street-address-electoral.prj" 
-if errorlevel 1 echo Wrong projection  & pause & eexit 1
+if errorlevel 1 echo Wrong projection  & pause & exit 1
 time /t
 %nzogps_psql_bin%shp2pgsql -d -g the_geom -D -s4167 "..\..\LinzDataService\lds-nz-street-address-electoral-SHP\nz-street-address-electoral.shp"  nz-street-address-electoral > nz-street-address.sql
 time /t

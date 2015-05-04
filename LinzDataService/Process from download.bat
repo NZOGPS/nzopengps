666d3429e0830556f7dd66d735643f08a9f04924
Gary Turner <gary.c.turner@gmail.com>
2015-05-04 22:36:51 +0000

    Numbering - doing roads with lots of missing numbers

--- LinzDataService/Process from download.bat
+++ LinzDataService/Process from download.bat
@@ -5,8 +5,10 @@ call ..\setlocals.bat
 start tortoiseproc /command:update /path:".." /closeonend:1
 @if not exist %nzogps_download%"lds-new-zealand-2layers-SHP.zip" echo new shapefile download not found. & goto :eof
 move %nzogps_download%"lds-new-zealand-2layers-SHP.zip" .
-%nzogps_unzip_cmd% lds-new-zealand-2layers-SHP.zip -olds-nz-street-address-electoral-SHP *\nz-street-address-electoral-?.*
+del  /Y lds-nz-street-address-electoral-SHP\*.*
+%nzogps_unzip_cmd% lds-new-zealand-2layers-SHP.zip -olds-nz-street-address-electoral-SHP *\nz-street-address-electoral*.*
 if %ERRORLEVEL% GTR 0 echo Street address files not found in zip file & goto :eof
+del  /Y lds-nz-road-centre-line-electoral-SHP\*.*
 %nzogps_unzip_cmd% lds-new-zealand-2layers-SHP.zip -olds-nz-road-centre-line-electoral-SHP *\nz-road-centre-line-electoral.*
 if %ERRORLEVEL% GTR 0 echo Road centre line  files not found in zip file & goto :eof
 %nzogps_perl_cmd% renzip.pl
--- scripts/postgres/update.bat
+++ scripts/postgres/update.bat
@@ -1,14 +1,17 @@
 @echo on
 setlocal
+set nzogps_sae=..\..\LinzDataService\lds-nz-street-address-electoral-SHP
+
 if not exist ..\..\setlocals.bat echo setlocals.bat not found. You need to copy and customise the sample file & goto :eof
 call ..\..\setlocals.bat
 net start %nzogps_psql_svc%
-grep -q  "^GEOGCS\[\"GCS_NZGD_2000" "..\..\LinzDataService\lds-nz-street-address-electoral-SHP\nz-street-address-electoral-2.prj" 
+grep -q  "^GEOGCS\[\"GCS_NZGD_2000" "%nzogps_sae%\nz-street-address-electoral-2.prj" 
 if errorlevel 1 echo Wrong projection  & pause & exit 1
-if exist "..\..\LinzDataService\lds-nz-street-address-electoral-SHP\nz-street-address-electoral-4.shp" echo 4th shapefile found & pause & exit 1
+if exist "%nzogps_sae%\nz-street-address-electoral-4.shp" echo 4th shapefile found & pause & exit 1
+del nz-street-address*.sql
 time /t
-%nzogps_psql_bin%shp2pgsql -d -g the_geom -D -s4167 "..\..\LinzDataService\lds-nz-street-address-electoral-SHP\nz-street-address-electoral-2.shp"  nz-street-address-electoral > nz-street-address-2.sql
-%nzogps_psql_bin%shp2pgsql -a -g the_geom -D -s4167 "..\..\LinzDataService\lds-nz-street-address-electoral-SHP\nz-street-address-electoral-3.shp"  nz-street-address-electoral > nz-street-address-3.sql
+%nzogps_psql_bin%shp2pgsql -d -g the_geom -D -s4167 "%nzogps_sae%\nz-street-address-electoral-2.shp"  nz-street-address-electoral > nz-street-address-2.sql
+%nzogps_psql_bin%shp2pgsql -a -g the_geom -D -s4167 "%nzogps_sae%\nz-street-address-electoral-3.shp"  nz-street-address-electoral > nz-street-address-3.sql
 time /t
 %nzogps_psql_bin%psql -U postgres -d nzopengps < nz-street-address-2.sql
 %nzogps_psql_bin%psql -U postgres -d nzopengps < nz-street-address-3.sql

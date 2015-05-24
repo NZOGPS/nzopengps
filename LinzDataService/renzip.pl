7794479635d9d72cdbcbe2442b8e781d2e2ea4dc
Gary Turner <gary.c.turner@gmail.com>
2015-05-24 03:25:10 +0000

    urk! only now discovered my renaming was off by one month...

--- LinzDataService/renzip.pl
+++ LinzDataService/renzip.pl
@@ -5,7 +5,7 @@ my $fn = shift || "lds-new-zealand-2layers-SHP.zip";
 die "$fn not found\n" if not -f $fn;
 my @stats = stat $fn;
 my @lt = localtime $stats[9];
-my $datebit = sprintf "-%04d-%02d-%02d",$lt[5]+1900,$lt[4],$lt[3];
+my $datebit = sprintf "-%04d-%02d-%02d",$lt[5]+1900,$lt[4]+1,$lt[3];
 my $nfn = basename($fn);
 
 $nfn =~ s/\.zip/$datebit\.zip/;

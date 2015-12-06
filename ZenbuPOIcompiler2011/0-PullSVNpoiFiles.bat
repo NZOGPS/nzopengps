f53c98a850d330aeb3c8d75b25bd51860ed21a17
garyt-nz <g@elizr.com>
2015-12-06 13:15:43 +1300

    add --force to bunzip command

--- ZenbuPOIcompiler2011/0-PullSVNpoiFiles.bat
+++ ZenbuPOIcompiler2011/0-PullSVNpoiFiles.bat
@@ -3,5 +3,5 @@ rem svn up
 if (%nzogps_base%)==() call ..\setlocals.bat
 %nzogps_git% pull
 %nzogps_curl% -u %nzogps_zenbu_user% -o zenbuNZ.csv.bz2 http://www.zenbu.co.nz/export/all
-%nzogps_bunzip% zenbuNZ.csv.bz2
+%nzogps_bunzip% --force zenbuNZ.csv.bz2
 pause

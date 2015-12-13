bb5064fbbc4d85f14578d5feb4f10567310bffb8
garyt-nz <g@elizr.com>
2015-12-13 18:32:48 +1300

    LINZ updates, tweaks to Zenbu processing, Start new WEKA sth

--- ZenbuPOIcompiler2011/2-SVN commit.bat
+++ ZenbuPOIcompiler2011/2-SVN commit.bat
@@ -1,8 +1,5 @@
-rem cd ..
-rem svn st
 if (%nzogps_base%)==() call setlocals.bat
-%nzogps_git% commit -m "POI Update" -uno NZPOIs*.mp
+%nzogps_git% commit -m "POI Update" -uno ..\NZPOIs*.mp
 pause
-rem svn commit -m "POI Update"
 %nzogps_git% push
 pause
--- ZenbuPOIcompiler2011/3-RebuildCategories.bat
+++ ZenbuPOIcompiler2011/3-RebuildCategories.bat
@@ -1,2 +1,7 @@
+if (%nzogps_base%)==() call setlocals.bat
 %nzogps_ruby_cmd% rebuild_category_files.rb
+pause
+%nzogps_git% commit -m "Zenbu Category Update" -uno ..\ZenbuPOIcategories2011\*.txt
+pause
+%nzogps_git% push
 pause

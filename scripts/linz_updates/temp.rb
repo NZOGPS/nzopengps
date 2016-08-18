0c0a823b46f3550a0ae6281ee6fac9dbb28110d0
garyt-nz <g@elizr.com>
2016-08-18 18:14:56 +1200

    Add in roads to change processing

--- scripts/linz_updates/temp.rb
+++ scripts/linz_updates/temp.rb
@@ -1,6 +1,8 @@
 LINZ_URL="https://data.linz.govt.nz/services;"
 FN_779="layer_779_cs"
+FN_818="layer_818_cs"
 ADDR_TABLE="\"nz-street-address-electoral-2016-07-16\""
+ROAD_TABLE="\"nz-road-centre-line-electoral-2016-07-16\""
 
 def get_linz_updates()
 	#need to check these for validity
@@ -12,7 +14,6 @@ def get_linz_updates()
 	#curl_cmd = "echo #{curl_cmd}"
 
 	from_date = "2016-07-17T12:00:00"
-	#to_date = "2016-08-13T02:06:09.394426"
 
 	nztime = Time.new
 	to_date = nztime.utc.strftime("%FT%T")
@@ -25,8 +26,8 @@ def get_linz_updates()
 	layer = 779
 	system("#{curl_cmd} -o #{FN_779}.csv #{url1}#{layer}#{url2}#{layer}#{url3}")
 	layer = 818
-	#system("#{curl_cmd} -o layer-#{layer}-cs.csv #{url1}#{layer}#{url2}#{layer}#{url3}")
-	layer = 793
+	system("#{curl_cmd} -o #{FN_818}.csv #{url1}#{layer}#{url2}#{layer}#{url3}")
+	#layer = 793
 	#system("#{curl_cmd} -o layer-#{layer}-cs.csv #{url1}#{layer}#{url2}#{layer}#{url3}")
 end
 
@@ -70,20 +71,27 @@ def put_csv_in_postgres()
 	ogr_cmd = ENV['nzogps_ogr2ogr']
 	abort("Processing aborted! nzogps_ogr2ogrl environment variable not set!") if !ogr_cmd
 	abort("Processing aborted! csv definition file #{FN_779}.csvt not found!") if !File.file?("#{FN_779}.csvt")
+	abort("Processing aborted! csv definition file #{FN_818}.csvt not found!") if !File.file?("#{FN_818}.csvt")
 	
 	system("#{ogr_cmd} --config PG_USE_COPY TRUE -overwrite -f \"PostgreSQL\" \"PG:host=localhost user=postgres  dbname=nzopengps\"  #{FN_779}.csv")
+	system("#{ogr_cmd} --config PG_USE_COPY TRUE -overwrite -f \"PostgreSQL\" \"PG:host=localhost user=postgres  dbname=nzopengps\"  #{FN_818}.csv")
 
 	@conn.exec "ALTER TABLE #{FN_779} ADD COLUMN is_odd boolean"
-	@conn.exec "SELECT AddGeometryColumn('','#{FN_779}','the_geom',4167,'POINT',2)"
 	@conn.exec "UPDATE #{FN_779} SET is_odd = MOD(range_low,2) = 1"
+	@conn.exec "SELECT AddGeometryColumn('','#{FN_779}','the_geom',4167,'POINT',2)"
 	@conn.exec "UPDATE #{FN_779} SET the_geom = ST_FlipCoordinates(ST_GeomFromText(shape,4167))" #ogr doesn't convert WKT, and also coord order is reversed
+	@conn.exec "VACUUM ANALYSE #{FN_779}"
+
+	@conn.exec "SELECT AddGeometryColumn('','#{FN_818}','the_geom',4167,'MultiLineString',2)"
+	@conn.exec "UPDATE #{FN_818} SET the_geom = ST_FlipCoordinates(ST_GeomFromText(shape,4167))" #ogr doesn't convert WKT, and also coord order is reversed
+	@conn.exec "VACUUM ANALYSE #{FN_818}"
 end
 
 def check_for_errors()
 	rs = @conn.exec "SELECT sae.gid, cs.id, cs.address, cs.__change__ "\
 	"from #{FN_779} cs join #{ADDR_TABLE} sae on sae.id = cs.id where cs.__change__ = 'INSERT'"
 	if rs.count > 0 then
-		STDERR.puts rs.count.to_s + " ID(s) in updates for addition already exists in database"
+		STDERR.puts rs.count.to_s + " ID(s) in address updates for addition already exists in database"
 		STDERR.puts "\tFirst ID: " + rs.first['id'] + " - " + rs.first['address']
 		abort("Processing aborted")
 	end
@@ -91,10 +99,27 @@ def check_for_errors()
 	rs = @conn.exec "SELECT cs.id, cs.address, cs.__change__ from #{FN_779} cs "\
 		"where cs.__change__ in ('UPDATE','DELETE') and not exists( select id from #{ADDR_TABLE} sae where sae.id = cs.id)"
 	if rs.count > 0 then
-		STDERR.puts rs.count.to_s + " ID(s) in updates for modification/deletion do not already exist in database"
+		STDERR.puts rs.count.to_s + " ID(s) in address updates for modification/deletion do not already exist in database"
 		STDERR.puts "\tFirst ID: " + rs.first['id'] + " - " + rs.first['address']
 		abort("Processing aborted")
 	end
+
+	rs = @conn.exec "SELECT cs.id, cs.name, cs.__change__ "\
+	"from #{FN_818} cs join #{ROAD_TABLE} rcl on rcl.id = cs.id where cs.__change__ = 'INSERT'"
+	if rs.count > 0 then
+		STDERR.puts rs.count.to_s + " ID(s) in road updates for addition already exists in database"
+		STDERR.puts "\tFirst ID: " + rs.first['id'] + " - " + rs.first['name']
+		abort("Processing aborted")
+	end
+
+	rs = @conn.exec "SELECT cs.id, cs.name, cs.__change__ from #{FN_818} cs "\
+		"where cs.__change__ in ('UPDATE','DELETE') and not exists( select id from #{ROAD_TABLE} rcl where rcl.id = cs.id)"
+	if rs.count > 0 then
+		STDERR.puts rs.count.to_s + " ID(s) in road updates for modification/deletion do not already exist in database"
+		STDERR.puts "\tFirst ID: " + rs.first['id'] + " - " + rs.first['name']
+		abort("Processing aborted")
+	end
+
 end
 
 def do_updates()
@@ -105,13 +130,27 @@ def do_updates()
 		"SELECT id,rna_id,rcl_id,address,house_number,range_low,range_high,is_odd,road_name,locality,territorial_authority,road_name_utf8,address_utf8,locality_utf8,the_geom "\
 		"FROM #{FN_779} where __change__ = 'INSERT'"
 
-		@conn.exec "UPDATE #{ADDR_TABLE} sae "\
+	@conn.exec "UPDATE #{ADDR_TABLE} sae "\
 		"SET rna_id=subquery.rna_id, rcl_id=subquery.rcl_id, address=subquery.address, house_numb=subquery.house_number,"\
 			"range_low=subquery.range_low, range_high=subquery.range_high,is_odd=subquery.is_odd,"\
 			"road_name=subquery.road_name, locality=subquery.locality, territoria=subquery.territorial_authority,"\
 			"road_name_=subquery.road_name_utf8, address_ut=subquery.address_utf8, locality_u=subquery.locality_utf8, the_geom=subquery.the_geom "\
 		"FROM (SELECT id,rna_id,rcl_id,address,house_number,range_low,range_high,is_odd,road_name,locality,territorial_authority,road_name_utf8,address_utf8,locality_utf8,the_geom "\
-		"FROM layer_779_cs ) AS subquery WHERE sae.id=subquery.id"
+		"FROM #{FN_779} ) AS subquery WHERE sae.id=subquery.id and cs.__change__ = 'UPDATE'"
+
+	@conn.exec "DELETE FROM #{ROAD_TABLE} rcl USING #{FN_818} cs WHERE rcl.id = cs.id AND cs.__change__ = 'DELETE'"
+
+	@conn.exec "INSERT INTO #{ROAD_TABLE} "\
+		"(id,name,locality,territoria,name_utf8,locality_u,the_geom)"\
+		"SELECT id,name,locality,territorial_authority,name_utf8,locality_utf8,the_geom "\
+		"FROM #{FN_818} where __change__ = 'INSERT'"
+
+	@conn.exec "UPDATE #{ROAD_TABLE} rcl "\
+		"SET id=subquery.id, name=subquery.name, locality=subquery.locality, territoria=subquery.territorial_authority, "\
+			"name_utf8=subquery.name_utf8, locality_u=subquery.locality_utf8, the_geom=subquery.the_geom "\
+		"FROM (SELECT id,name,locality,territorial_authority,name_utf8,locality_utf8,the_geom "\
+		"FROM #{FN_818} ) AS subquery WHERE rcl.id=subquery.id and cs.__change__ = 'UPDATE'"
+
 end
 
 get_linz_updates()

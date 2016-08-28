9e66ea47f655e7d5fb186404ff6b0c64d3ec67db
garyt-nz <g@elizr.com>
2016-08-28 14:27:39 +1200

    added cmdline parameters

--- scripts/linz_updates/temp.rb
+++ scripts/linz_updates/temp.rb
@@ -1,10 +1,51 @@
+require 'optparse'
+require 'optparse/time'
+require 'optparse/date'
+
 LINZ_URL="https://data.linz.govt.nz/services;"
 FN_779="layer_779_cs"
 FN_818="layer_818_cs"
+FN_793="layer_793_cs"
+
 ADDR_TABLE="\"nz-street-address-electoral-2016-07-16\""
 ROAD_TABLE="\"nz-road-centre-line-electoral-2016-07-16\""
 
-def get_linz_updates()
+options = {:download => 1, :postgres => 1, :updates => 1, :from => "2016-07-17T12:00:00", :until => nil}
+
+def do_options(options)
+	parser = OptionParser.new do|opts|
+		opts.banner = "Usage: #{$0} [options]"
+		opts.on('-d', '--nodownload', 'Don\'t download') do |nodown|
+			options[:download] = nil;
+		end
+
+		opts.on('-p', '--nopostgres', 'Don\'t load into postgres') do |nopost|
+			options[:postgres] = nil;
+		end
+
+		opts.on('-u', '--noupdate', 'Don\'t do updates in postgres') do |noupd|
+			options[:updates] = nil;
+		end
+
+		opts.on("-f", "--from FROM", DateTime, "Specify a start time/date") do |from|
+			p from
+			options[:from] = from.strftime("%FT%T")
+		end
+
+		opts.on("-t", "--to TOTIME", DateTime, "Specify an until time/date") do |to|
+			options[:until] = to.strftime("%FT%T")
+		end
+
+		opts.on('-h', '--help', 'Displays Help') do
+			puts opts
+			exit
+		end
+	end
+
+	parser.parse!
+end
+
+def get_linz_updates(options)
 	#need to check these for validity
 
 	linz_key = ENV['nzogps_linz_api_key']
@@ -13,22 +54,27 @@ def get_linz_updates()
 
 	#curl_cmd = "echo #{curl_cmd}"
 
-	from_date = "2016-07-17T12:00:00"
+	#from_date = "2016-07-17T12:00:00"
 
-	nztime = Time.new
-	to_date = nztime.utc.strftime("%FT%T")
+	if (options[:until])
+		to_date = options[:until]
+	else
+		nztime = Time.new
+		to_date = nztime.utc.strftime("%FT%T")
+	end
 
 	url1 = "#{LINZ_URL}key=#{linz_key}/wfs/layer-"
 	url2 = "-changeset?SERVICE=WFS^&VERSION=2.0.0^&REQUEST=GetFeature^&typeNames=layer-"
-	url3 = "-changeset^&viewparams=from:#{from_date}Z;to:#{to_date}Z^&outputFormat=csv"
+	url3 = "-changeset^&viewparams=from:#{options[:from]}Z;to:#{to_date}Z^&outputFormat=csv"
+	puts url3
 
 	#need to add checks for valid return?
 	layer = 779
 	system("#{curl_cmd} -o #{FN_779}.csv #{url1}#{layer}#{url2}#{layer}#{url3}")
 	layer = 818
 	system("#{curl_cmd} -o #{FN_818}.csv #{url1}#{layer}#{url2}#{layer}#{url3}")
-	#layer = 793
-	#system("#{curl_cmd} -o layer-#{layer}-cs.csv #{url1}#{layer}#{url2}#{layer}#{url3}")
+	layer = 793
+	system("#{curl_cmd} -o #{FN_793}.csv #{url1}#{layer}#{url2}#{layer}#{url3}")
 end
 
 def pg_connect()
@@ -68,23 +114,41 @@ def pg_connect()
 end
 
 def put_csv_in_postgres()
+	#find ogr command from environment
 	ogr_cmd = ENV['nzogps_ogr2ogr']
 	abort("Processing aborted! nzogps_ogr2ogrl environment variable not set!") if !ogr_cmd
+
+	#check that csvt files with column types exist
 	abort("Processing aborted! csv definition file #{FN_779}.csvt not found!") if !File.file?("#{FN_779}.csvt")
 	abort("Processing aborted! csv definition file #{FN_818}.csvt not found!") if !File.file?("#{FN_818}.csvt")
-	
+	abort("Processing aborted! csv definition file #{FN_793}.csvt not found!") if !File.file?("#{FN_793}.csvt")
+
+	#use ogr to import csv files into postgres
 	system("#{ogr_cmd} --config PG_USE_COPY TRUE -overwrite -f \"PostgreSQL\" \"PG:host=localhost user=postgres  dbname=nzopengps\"  #{FN_779}.csv")
 	system("#{ogr_cmd} --config PG_USE_COPY TRUE -overwrite -f \"PostgreSQL\" \"PG:host=localhost user=postgres  dbname=nzopengps\"  #{FN_818}.csv")
+	system("#{ogr_cmd} --config PG_USE_COPY TRUE -overwrite -f \"PostgreSQL\" \"PG:host=localhost user=postgres  dbname=nzopengps\"  #{FN_793}.csv")
 
+	#do postprocessing of addresses in postgres
 	@conn.exec "ALTER TABLE #{FN_779} ADD COLUMN is_odd boolean"
 	@conn.exec "UPDATE #{FN_779} SET is_odd = MOD(range_low,2) = 1"
 	@conn.exec "SELECT AddGeometryColumn('','#{FN_779}','the_geom',4167,'POINT',2)"
-	@conn.exec "UPDATE #{FN_779} SET the_geom = ST_FlipCoordinates(ST_GeomFromText(shape,4167))" #ogr doesn't convert WKT, and also coord order is reversed
+	@conn.exec "UPDATE #{FN_779} SET the_geom = ST_FlipCoordinates(ST_GeomFromText(shape,4167))" #older versions of ogr don't convert WKT, also coord order is reversed
 	@conn.exec "VACUUM ANALYSE #{FN_779}"
 
+	#do postprocessing of road centrelines in postgres
 	@conn.exec "SELECT AddGeometryColumn('','#{FN_818}','the_geom',4167,'MultiLineString',2)"
-	@conn.exec "UPDATE #{FN_818} SET the_geom = ST_FlipCoordinates(ST_GeomFromText(shape,4167))" #ogr doesn't convert WKT, and also coord order is reversed
+	@conn.exec "UPDATE #{FN_818} SET the_geom = ST_FlipCoordinates(ST_GeomFromText(shape,4167))" #older versions of ogr don't convert WKT, also coord order is reversed
 	@conn.exec "VACUUM ANALYSE #{FN_818}"
+
+	#do postprocessing of road centrelines sections in postgres
+	@conn.exec "SELECT AddGeometryColumn('','#{FN_793}','the_geom',4167,'LineString',2)"
+	@conn.exec "UPDATE #{FN_793} SET the_geom = ST_FlipCoordinates(ST_GeomFromText(shape,4167))" #older versions of ogr don't convert WKT, also coord order is reversed
+	@conn.exec "VACUUM ANALYSE #{FN_793}"
+
+	#just export 793 for now
+	system("del #{FN_793}.gpx")
+	system("#{ogr_cmd} -overwrite -f GPX #{FN_793}.gpx \"PG:user=postgres dbname=nzopengps tables=#{FN_793}\"")
+
 end
 
 def check_for_errors()
@@ -152,10 +216,19 @@ def do_updates()
 		"FROM #{FN_818} where __change__ = 'UPDATE') AS subquery WHERE rcl.id=subquery.id"
 
 end
+do_options(options)
 
-get_linz_updates()
+if (options[:download]) 
+	get_linz_updates(options)
+end
+
 pg_connect()
-put_csv_in_postgres()
-check_for_errors()
-do_updates()
 
+if (options[:postgres]) 
+	put_csv_in_postgres()
+	check_for_errors()
+end
+
+if (options[:updates]) 
+	do_updates()
+end

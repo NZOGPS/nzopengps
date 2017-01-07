a4245ad40998b20ec746604b9a9053b12d2b2566
garyt-nz <g@elizr.com>
2017-01-07 20:03:03 +1300

    Change Zenbu download URL to be https

--- ZenbuPOIcompiler2011/0-PullSVNpoiFiles.bat
+++ ZenbuPOIcompiler2011/0-PullSVNpoiFiles.bat
@@ -1,5 +1,5 @@
 if not defined nzogps_git call ..\setlocals.bat
 %nzogps_git% pull
-%nzogps_curl% -u %nzogps_zenbu_user% -o zenbuNZ.csv.bz2 http://www.zenbu.co.nz/export/all
+%nzogps_curl% -u %nzogps_zenbu_user% -o zenbuNZ.csv.bz2 https://www.zenbu.co.nz/export/all
 %nzogps_bunzip% --force zenbuNZ.csv.bz2
 pause

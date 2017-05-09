a7e9700933fdfa89009e8075c044233dce66488e
garyt-nz <g@elizr.com>
2017-05-10 00:30:56 +1200

    Fix a few wrongside files, do a run.

--- scripts/Wrongside/Process_tile.bat
+++ scripts/Wrongside/Process_tile.bat
@@ -17,8 +17,8 @@ if /i %1 equ Southland	%nzogps_psqlc% -c "drop table if exists %1_nums; Create t
 %nzogps_psqlc% -v numstable=%1_nums  -f postpro-nums.sql
 
 mp_2_n_sql.pl %nzogps_base%\%1.mp
-%nzogps_psqlc%   -f %1_numberlines.sql
+%nzogps_psqlc% -f %1_numberlines.sql
 %nzogps_psqlc% -v linestable=%1_numberlines -v distance=100 -f postpro-lines.sql
-%nzogps_psqlc% -v linestable=%1_numberlines  -v numstable=%1_nums -v outfile='%nzogps_base%\scripts\wrongside\%1-WrongSide.csv' -f intersect.sql
+%nzogps_psqlc% -v linestable=%1_numberlines -v numstable=%1_nums -v outfile='%nzogps_base%\scripts\wrongside\%1-WrongSide.csv' -f intersect.sql
 %nzogps_psqlc% -v linestable=%1_numberlines -v distance=500 -v outfile='%nzogps_base%\scripts\wrongside\%1-Sparsest.csv' -f Sparsest.sql
 perl wrongsidereport.pl %1 > %1-Wrongside-report.txt
---
+++ scripts/Wrongside/Redo_tile.bat
@@ -0,0 +1,12 @@
+@echo off
+if xx%1xx==xxxx echo tile not specified. Usage process tile tilename & goto :eof
+if xx%nzogps_psql_bin%xx==xxxx echo NZOGPS Environment Variables not set - run setlocals.bat & goto :eof
+if not exist %nzogps_base%\%1.mp echo %nzogps_base%\%1.mp not found & goto :eof
+set nzogps_psqlc=%nzogps_psql_bin%psql -U postgres -d nzopengps
+
+mp_2_n_sql.pl %nzogps_base%\%1.mp
+%nzogps_psqlc%   -f %1_numberlines.sql
+%nzogps_psqlc% -v linestable=%1_numberlines -v distance=100 -f postpro-lines.sql
+%nzogps_psqlc% -v linestable=%1_numberlines  -v numstable=%1_nums -v outfile='%nzogps_base%\scripts\wrongside\%1-WrongSide.csv' -f intersect.sql
+%nzogps_psqlc% -v linestable=%1_numberlines -v distance=200 -v outfile='%nzogps_base%\scripts\wrongside\%1-Sparsest.csv' -f Sparsest.sql
+perl wrongsidereport.pl %1 > %1-Wrongside-report.txt

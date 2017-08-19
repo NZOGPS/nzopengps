fb269ec69b9ec63fffef67d8396d1a58479230a5
garyt-nz <g@elizr.com>
2017-08-19 19:30:23 +1200

    Remap linzids for Wellington, Waikato. Updated script.

--- scripts/remap_linzids.pl
+++ scripts/remap_linzids.pl
@@ -1,14 +1,15 @@
 use strict;
+use warnings;
 use File::Basename;
-	
+
 my $lds = "..\\LinzDataService\\";
 my $pdn = "${lds}PaperRoads\\";
 # my $mappingfilename = "${lds}rna_mappings.csv";
 my $mappingfilename = "${lds}new_street_address_ids.csv";
 my %idmap;
 my $cnt;
-my $oldfn;
-my $newfn;
+my $filfn;
+my $bakfn;
 my $basefile;
 my $basedir;
 my $basesuff;
@@ -19,34 +20,34 @@ my $dienomatch = 0;
 $idmap{0}=0;
 
 sub do_paper_file{
-	if (-r $oldfn){
+	if (-r $filfn){
 		$cnt = 0;
-		$newfn = $oldfn;
-		$newfn =~ s!\.(?=[^.]*$)!\.remapped\.!;
-		print "Creating new file $newfn\n";
-		open INF, $oldfn or die "Cannot open Paper road file $oldfn\n";
-		open MAPPEDF,">",$newfn or die "Cannot create new map file $newfn\n";
+		$bakfn = $filfn;
+		$bakfn =~ s!\.(?=[^.]*$)!\.old_ids\.!;
+		print "Renaming $mode file as $bakfn\n";
+		rename ($filfn, $bakfn) or die "Failed to rename $filfn as $bakfn\n";
+		open INF, $bakfn or die "Cannot find paper $bakfn\n";
+		open PAPERF,">",$filfn or die "Cannot create new $mode file $filfn\n";
 		while (<INF>){
 			if (/(\d+)\t/){
-				$cnt++;
 				$oldid = $1;
 				if (defined($idmap{$oldid})) {
 					s/^$oldid/$idmap{$oldid}/;
 					$cnt++;
 				} else {
 					if ($dienomatch){
-						print "ERROR! linzid map for $1 not found! File $oldfn line $.\n";
+						print "ERROR! linzid map for $1 not found! File $bakfn line $.\n";
 						s/^$oldid/###$oldid### - no new mapping for linzid found/;
 					}
 				}
 			}
-			print MAPPEDF $_;
+			print PAPERF $_;
 		}
 		close INF;
-		close MAPPEDF;
+		close PAPERF;
 		print "$cnt linzids remapped\n";
 	} else {
-		print "$oldfn not found. Skipped\n";
+		print "$filfn not found. Skipped\n";
 	}
 }
 
@@ -64,50 +65,49 @@ print "$cnt mappings set\n";
 close IDF;
 $cnt = 0;
 
-die "No filename specified" if ($ARGV[0] eq "");
-$oldfn = $ARGV[0];
-($basefile, $basedir, $basesuff) = fileparse($oldfn,qr/\.[^.]*/);
+die "No filename specified" if (!defined $ARGV[0]);
+$filfn = $ARGV[0];
+($basefile, $basedir, $basesuff) = fileparse($filfn,qr/\.[^.]*/);
 
-if (1){
-	open INF, $oldfn or die "Cannot find map $oldfn\n";
-	$newfn = $oldfn;
-	$newfn =~ s!\.(?=[^.]*$)!\.remapped\.!;
-	print "Creating new file $newfn\n";
-	open MAPPEDF,">",$newfn or die "Cannot create new map file $newfn\n";
-	while (<INF>){
-		if (/linzid\d?=(\d+)/){
-			$oldid = $1;
-			if (defined($idmap{$oldid})) {
-				s/$oldid/$idmap{$oldid}/;
-				$cnt++;
-			} else {
-				if ($dienomatch){
-					print "ERROR! linzid map for $1 not found! File $oldfn line $.\n";
-					s/$oldid/###$oldid### - no new mapping for linzid found/;
-				}
-			}  
-		}
-		print MAPPEDF $_;
+$bakfn = $filfn;
+$bakfn =~ s!\.(?=[^.]*$)!\.old_ids\.!;
+print "Renaming map file as $bakfn\n";
+rename ($filfn, $bakfn) or die "Failed to rename $filfn as $bakfn\n";
+open INF, $bakfn or die "Cannot find map $bakfn\n";
+open MAPPEDF,">",$filfn or die "Cannot create new map file $filfn\n";
+while (<INF>){
+	if (/linzid\d?=(\d+)/){
+		$oldid = $1;
+		if (defined($idmap{$oldid})) {
+			s/$oldid/$idmap{$oldid}/;
+			$cnt++;
+		} else {
+			if ($dienomatch){
+				print "ERROR! linzid map for $1 not found! File $bakfn line $.\n";
+				s/$oldid/###$oldid### - no new mapping for linzid found/;
+			}
+		}  
 	}
-	close INF;
-	close MAPPEDF;
-	print "$cnt linzids remapped\n";
-	
-	#Paper roads
-	
-	$oldfn = "${pdn}${basefile}.txt";
-	$mode = "Paper road";
-	do_paper_file;
-	
-	$oldfn = "${pdn}${basefile}PaperNumbers.txt";
-	$mode = "Paper Numbers";
-	do_paper_file;
+	print MAPPEDF $_;
 }
+close INF;
+close MAPPEDF;
+print "$cnt linzids remapped\n";
 
-$oldfn = "${pdn}${basefile}-LINZWrongSide.txt";
+#Paper roads
+
+$filfn = "${pdn}${basefile}.txt";
+$mode = "Paper road";
+do_paper_file;
+
+$filfn = "${pdn}${basefile}PaperNumbers.txt";
+$mode = "Paper Numbers";
+do_paper_file;
+
+$filfn = "${pdn}${basefile}-LINZWrongSide.txt";
 $mode = "LINZ Wrongside";
 do_paper_file;
 
-$oldfn = "${pdn}${basefile}-WrongSide.txt";
+$filfn = "${pdn}${basefile}-WrongSide.txt";
 $mode = "Wrongside";
 do_paper_file;

ac0320bfae0bc5809547209795ae6a4b1003fed0
garyt-nz <g@elizr.com>
2017-10-28 12:39:05 +1300

    Little script to zip and rename Mac installer

---
+++ installer/uppercase/ZipUCaseMac.bat
@@ -0,0 +1,3 @@
+@echo off
+for /f "delims=_ tokens=1" %%d in ( 'dir /b /od *.exe') do set nzogps_latest_compile=%%d
+7z a %NZOGPS_LatestCompile%_Free_Uppercase_Mac_Open_GPS_NZ_Autorouting.gmapi.zip "d:\garmin\Free Open GPS NZ Autorouting.gmapi"

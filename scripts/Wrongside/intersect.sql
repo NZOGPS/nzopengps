faf071d8171de5c22359f0f2bc62b8d2a7023973
garyt-nz <g@elizr.com>
2018-12-02 16:42:01 +1300

    Doing wrongsides Updated distance for sparse. Fixed bug in intersect.sql ( exporting UTF to csv instead of ascii) Fixed a few in wgtn/can.

--- scripts/Wrongside/intersect.sql
+++ scripts/Wrongside/intersect.sql
@@ -7,5 +7,5 @@ update :numstable set asnum_position=ST_LineInterpolatePoint(nztm_line,asnum_dis
 update :numstable set asnum_dist_err=ST_Distance(nztm,asnum_position);
 update :numstable set isect_side=1, isect_roadid=:linestable.gid from :linestable where rna_id=linzid and ST_Intersects(nztm,rightpoly);
 update :numstable set isect_side=-1, isect_roadid=:linestable.gid from :linestable where rna_id=linzid and ST_Intersects(nztm,leftpoly);
-copy(select gd2000_xcoord,gd2000_ycoord,full_address,'Wrong Side: ' || address_id from :numstable where asnum_roadid = isect_roadid and asnum_side*isect_side=-1 order by address_id) to :outfile with CSV;
+copy(select gd2000_xcoord,gd2000_ycoord,full_address_ascii,'Wrong Side: ' || address_id from :numstable where asnum_roadid = isect_roadid and asnum_side*isect_side=-1 order by address_id) to :outfile with CSV;
 select current_time;

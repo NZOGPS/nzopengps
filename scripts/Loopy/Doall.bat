b1ef06d830a6b91011e686e3c03588137b88981c
garyt-nz <g@elizr.com>
2021-03-25 18:02:32 +1300

    add Doall.bat

---
+++ scripts/Loopy/Doall.bat
@@ -0,0 +1,8 @@
+call process_tile northland
+call process_tile auckland
+call process_tile waikato
+call process_tile central
+call process_tile wellington
+call process_tile tasman
+call process_tile canterbury
+call process_tile southland

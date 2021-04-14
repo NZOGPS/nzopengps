c7291035c0f8dccd0e758fe7f24bacac185847f8
garyt-nz <g@elizr.com>
2021-04-14 13:37:39 +1200

    Added inline perl snippet to automatically lower case argument, and also remove file extension to argument for Loopy/Process_tile.bat

--- scripts/Loopy/Process_tile.bat
+++ scripts/Loopy/Process_tile.bat
@@ -1,10 +1,12 @@
 @echo off
+setlocal
 if xx%1xx==xxxx echo tile not specified. Usage process tile tilename & goto :eof
 if xx%nzogps_psql_bin%xx==xxxx echo NZOGPS Environment Variables not set - run setlocals.bat & goto :eof
-if not exist %nzogps_base%\%1.mp echo %nzogps_base%\%1.mp not found & goto :eof
+for /F "usebackq" %%f IN (`%nzogps_perl_cmd% -e "print lc(pop)" %1`) DO set loopy_fn=%%~nf
+if not exist %nzogps_base%\%loopy_fn%.mp echo %nzogps_base%\%loopy_fn%.mp not found & goto :eof
 set nzogps_psqlc=%nzogps_psql_bin%psql -U postgres -d nzopengps
-echo Loop finding processing %1
+echo Loop finding processing %loopy_fn%
 
-..\mp_2_sql.pl %nzogps_base%\%1.mp
-%nzogps_psqlc% -f %1.sql
-%nzogps_psqlc% -v linestable=%1 -v nodedtable=%1_noded_loops -v outfile='%nzogps_base%\scripts\loopy\%1-loops.csv' -f loopy1.sql
+..\mp_2_sql.pl %nzogps_base%\%loopy_fn%.mp
+%nzogps_psqlc% -f %loopy_fn%.sql
+%nzogps_psqlc% -v linestable=%loopy_fn% -v nodedtable=%loopy_fn%_noded_loops -v outfile='%nzogps_base%\scripts\loopy\%loopy_fn%-loops.csv' -f loopy1.sql

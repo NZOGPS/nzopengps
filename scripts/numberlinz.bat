66b93d40baf764c2ded4042c8ce1d429b357adb5
garyt-nz <g@elizr.com>
2022-08-07 15:07:58 +1200

    LINZ update Canterbury.mp Northland.mp Central.mp Waikato.mp Wellington.mp update scripts reorder files in scripts for my convenience!

--- scripts/numberlinz.bat
+++ scripts/numberlinz.bat
@@ -1,9 +1,9 @@
 set PROCESSING_LIBRARY=3
-%nzogps_ruby_cmd% parseMP.rb 1 %PROCESSING_LIBRARY%
-%nzogps_ruby_cmd% parseMP.rb 6 %PROCESSING_LIBRARY%
 %nzogps_ruby_cmd% parseMP.rb 8 %PROCESSING_LIBRARY%
 %nzogps_ruby_cmd% parseMP.rb 4 %PROCESSING_LIBRARY%
 %nzogps_ruby_cmd% parseMP.rb 5 %PROCESSING_LIBRARY%
+%nzogps_ruby_cmd% parseMP.rb 6 %PROCESSING_LIBRARY%
 %nzogps_ruby_cmd% parseMP.rb 7 %PROCESSING_LIBRARY%
 %nzogps_ruby_cmd% parseMP.rb 3 %PROCESSING_LIBRARY%
+%nzogps_ruby_cmd% parseMP.rb 1 %PROCESSING_LIBRARY%
 %nzogps_ruby_cmd% parseMP.rb 2 %PROCESSING_LIBRARY%

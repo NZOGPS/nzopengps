74e195bc948570f6e639220d3bfa16b54dfc4437
Gary Turner <g@elizr.com>
2023-07-07 20:20:33 +1200

    New koordinates layout -> new downloads

--- LinzDataService/Process from download.bat
+++ LinzDataService/Process from download.bat
@@ -2,9 +2,9 @@
 if not exist ..\setlocals.bat echo ..\setlocals.bat not found. You need to copy and customise the sample file & goto :eof
 if not defined nzogps_base call ..\setlocals.bat
 @echo on
-set nzogps_dl_fn=lds-new-zealand-4layers-CSV.zip
 %nzogps_git% pull -v
 
+set nzogps_dl_fn=lds-new-zealand-2layers-CSV.zip
 @if not exist %nzogps_download%\%nzogps_dl_fn% echo new CSV download %nzogps_dl_fn% not found. & goto :eof
 move %nzogps_download%\%nzogps_dl_fn% .
 
@@ -18,21 +18,25 @@ del lds-%nzogps_ex_pt%-CSV\%nzogps_ex_pt%.*
 %nzogps_unzip_cmd% %nzogps_dl_fn% -olds-%nzogps_ex_pt%-CSV %nzogps_ex_pt%\%nzogps_ex_pt%.*
 if not exist lds-%nzogps_ex_pt%-CSV\%nzogps_ex_pt%.csv echo Address files not found in zip file %nzogps_dl_fn% & goto :eof
 
+set nzogps_dl_fn=lds-aims-address-reference-CSV.zip
+@if not exist %nzogps_download%\%nzogps_dl_fn% echo new CSV download %nzogps_dl_fn% not found. & goto :eof
+move %nzogps_download%\%nzogps_dl_fn% .
 rem no nz in dir name
+rem no subdir in single file output-old
 set nzogps_ex_pt=AIMS-address-reference
 del lds-%nzogps_ex_pt%-CSV\%nzogps_ex_pt%.*
-%nzogps_unzip_cmd% %nzogps_dl_fn% -olds-%nzogps_ex_pt%-CSV %nzogps_ex_pt%\%nzogps_ex_pt%.*
+%nzogps_unzip_cmd% %nzogps_dl_fn% -olds-%nzogps_ex_pt%-CSV %nzogps_ex_pt%.*
 if not exist lds-%nzogps_ex_pt%-CSV\%nzogps_ex_pt%.csv echo Address reference files not found in zip file %nzogps_dl_fn% & goto :eof
 
 rem change from nz-street-address to nz-addresses - temp add 'deprecated' to fn
-set nzogps_ex_pt=nz-street-address
-del lds-%nzogps_ex_pt%-CSV\%nzogps_ex_pt%.*
-%nzogps_unzip_cmd% %nzogps_dl_fn% -olds-%nzogps_ex_pt%-CSV %nzogps_ex_pt%-deprecated\%nzogps_ex_pt%-deprecated.*
-if not exist lds-%nzogps_ex_pt%-CSV\%nzogps_ex_pt%-deprecated.csv echo old address files not found in zip file %nzogps_dl_fn% & goto :eof
+rem set nzogps_ex_pt=nz-street-address
+rem del lds-%nzogps_ex_pt%-CSV\%nzogps_ex_pt%.*
+rem %nzogps_unzip_cmd% %nzogps_dl_fn% -olds-%nzogps_ex_pt%-CSV %nzogps_ex_pt%-deprecated\%nzogps_ex_pt%-deprecated.*
+rem if not exist lds-%nzogps_ex_pt%-CSV\%nzogps_ex_pt%-deprecated.csv echo old address files not found in zip file %nzogps_dl_fn% & goto :eof
 rem fix filenames
-ren lds-%nzogps_ex_pt%-CSV\%nzogps_ex_pt%-deprecated.* %nzogps_ex_pt%.*
+rem ren lds-%nzogps_ex_pt%-CSV\%nzogps_ex_pt%-deprecated.* %nzogps_ex_pt%.*
 rem change reference to deprecated in vrt file - untested 
-%nzogps_perl_cmd% -i.bak -pe "s/-deprecated//" lds-%nzogps_ex_pt%-CSV\%nzogps_ex_pt%.vrt
+rem %nzogps_perl_cmd% -i.bak -pe "s/-deprecated//" lds-%nzogps_ex_pt%-CSV\%nzogps_ex_pt%.vrt
 
 %nzogps_perl_cmd% renzip.pl
 :xx

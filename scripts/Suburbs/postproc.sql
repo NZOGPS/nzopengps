14f4e58147c90b60a284c2c0af0f26ce32a8bbeb
Gary Turner <g@elizr.com>
2023-08-17 17:25:24 +1200

    Change folder structure

--- scripts/Suburbs/postproc.sql
+++ scripts/Suburbs/postproc.sql
@@ -4,5 +4,6 @@ CREATE INDEX nzsl_idx ON nz_suburbs_and_localities USING btree(id);
 
 ALTER TABLE nz_suburbs_and_localities ADD COLUMN watery boolean;
 ALTER TABLE nz_suburbs_and_localities ADD COLUMN nztm_geometry geometry(geometry,2193);
+CREATE INDEX nz_suburbs_and_localities_nztm_geometry_geom_idx ON public.nz_suburbs_and_localities USING gist(nztm_geometry);
 UPDATE nz_suburbs_and_localities SET watery = true where type = 'Coastal Bay' or type = 'Lake' or type = 'Inland Bay';
 UPDATE nz_suburbs_and_localities SET nztm_geometry = st_buffer(st_transform(wkb_geometry,2193),20);
--- scripts/Suburbs/wrong3.sql
+++ scripts/Suburbs/wrong3.sql
@@ -1,6 +1,7 @@
 -- 21:44 08/08/23 change name to name_ascii as macrons(?) caused issues
 -- 23:37 09/08/23 group by gid and array_agg the names to remove multiple points
-copy( select st_x(st_startpoint(the_geom)),st_y(st_startpoint(the_geom)), cn.label,'Shouldbe: '||array_to_string(array_agg(sb.name_ascii),', ')
+\timing
+copy( select st_x(st_lineinterpolatepoint(the_geom,0.01)),st_y(st_lineinterpolatepoint(the_geom,0.01)), cn.label,'Shouldbe: '|| array_to_string(array_agg(sb.name_ascii order by sb.name_ascii), ', '), max(st_distance(st_transform(the_geom,2193),nztm_geometry))::integer as distance
 	from :ntable cn 
 	join :ctable cc on cityidx = cityid 
 	join nz_suburbs_and_localities on nzslid = id
@@ -9,5 +10,7 @@ copy( select st_x(st_startpoint(the_geom)),st_y(st_startpoint(the_geom)), cn.lab
 		join nz_suburbs_and_localities sl 
 		on st_intersects(st_transform(the_geom,2193),nztm_geometry)) sb on sb.gid = cn.gid
 	where nzslid is not null 
-	and not st_intersects(st_transform(the_geom,2193),nztm_geometry)  group by cn.gid order by array_to_string(array_agg(sb.name_ascii),', ')
+	and not st_intersects(st_transform(the_geom,2193),nztm_geometry)  group by cn.gid 
+	having max(st_distance(st_transform(the_geom,2193),nztm_geometry)) > :mdist
+	order by max(st_distance(st_transform(the_geom,2193),nztm_geometry))
 ) to :outfile with CSV;

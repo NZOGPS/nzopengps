7ddcf9737c6e08a684074521000bad407fdf11f2
Gary Turner <g@elizr.com>
2023-11-30 18:50:43 +1300

    Wellington.mp city poi indexed

--- scripts/Suburbs/Code/nearpois.sql
+++ scripts/Suburbs/Code/nearpois.sql
@@ -3,8 +3,7 @@ copy( select st_x(the_geom),st_y(the_geom), cp.label,city,cityid,st_distance(st_
 	from :ptable cp 
 	join :ctable cc on cc.label = cp.label
 	where itype <= 4352 and cityidx=0
-	and st_contains(stbound,the_geom)
 	and not st_contains(stbound,the_geom)
 	and st_distance(st_transform(stbound,2193),st_transform(the_geom,2193)) <=1000
-	order by sd to :outfile with CSV;
-)
+	order by sd
+) to :outfile with CSV;

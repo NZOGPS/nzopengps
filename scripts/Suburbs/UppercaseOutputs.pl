3e750e03db302263ff41f3a5814bb11f9dbf895b
garyt-nz <g@elizr.com>
2025-12-19 00:11:21 +1300

    LINZ Tasman.mp Canterbury.mp, wrongside Waikato.mp Improved UppercaseOutputs.pl Updated reports

--- scripts/Suburbs/UppercaseOutputs.pl
+++ scripts/Suburbs/UppercaseOutputs.pl
@@ -1,12 +1,35 @@
 use strict;
-my @fs = glob "Outputs/*.csv"; 
+use File::Copy;
+
+my @ofs = glob "Outputs/*.csv"; 
 my $cnt = 0;
-for my $f(@fs){
-	my $f2=$f;
-	if( $f2 =~ s/Outputs\/([a-z])/Outputs\/\U$1/ ){
-		print "ren $f $f2\n";
+my $arg = $ARGV[0];
+my $checkit = 1;
+if ( defined $arg && $arg eq '-x'){
+	$checkit = 0;
+}
+
+for my $of(@ofs){
+	my $of2=$of;
+	if( $of2 =~ s/Outputs\/([a-z])/Outputs\/\U$1/ ){
+		if ($checkit) {
+			print "ren $of $of2\n";
+		} else {
+			move($of, $of2);
+			print "renamed $of to $of2\n";
+		}
 		$cnt++;
 	}
 }
-if ($cnt){print $cnt}else{print "none"}
-print " changed\n";
+
+if ($cnt){
+	if ($checkit){
+		print "$cnt files to rename\n";
+		print "Use $0 -x to actually change\n";
+	} else {
+		print "$cnt files renamed\n";
+	}
+} else {
+	print "no lowercase csv files in Outputs to change\n";
+}
+

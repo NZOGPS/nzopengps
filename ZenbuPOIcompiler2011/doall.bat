53792c5c24630318ec844126c84a3c44977ddb29
garyt-nz <g@elizr.com>
2026-07-25 13:13:33 +1200

    add doall.bat

---
+++ ZenbuPOIcompiler2011/doall.bat
@@ -0,0 +1,12 @@
+@echo off
+if not defined nzogps_git call ..\setlocals.bat
+%nzogps_git% pull
+%nzogps_curl% -L -u %nzogps_zenbu_user% -o zenbuNZ.csv.bz2 https://www.zenbu.co.nz/export/all
+%nzogps_bunzip% --force zenbuNZ.csv.bz2
+if not defined nzogps_ruby_cmd call ..\setlocals.bat
+%nzogps_ruby_cmd% process.rb
+%nzogps_git% commit -m "POI Update" -uno ..\NZPOIs*.mp
+%nzogps_git% push
+%nzogps_ruby_cmd% rebuild_category_files.rb
+%nzogps_git% commit -m "Zenbu Category Update" -uno ..\ZenbuPOIcategories2011\*.txt
+%nzogps_git% push

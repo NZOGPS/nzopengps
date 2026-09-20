a78061296f26cc2a69f61aaf46360951f2d0cd14
GaryT <g@elizr.com>
2026-09-21 00:12:44 +1200

    Renamed Wrongside2table to Wrongside LINZ udpate NSTWW

--- scripts/Wrongside2table/AllWrongS1.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-if xx%nzogps_psql_bin%xx==xxxx echo NZOGPS Environment Variables not set - run setlocals.bat & goto :eof
-title Wrongside WaiWel
-call Process_tile Waikato
-call Process_tile Wellington
-title Done wrongside WaiWel
